d56439d1606b257ca0b88ce7b8548c19033460a0
SeymaDemir <seymademir35@gmail.com>
2022-11-24 11:18:50 +0300

    tried destructing

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import MovieCard from "./MovieCard";
 
 function App() {
   const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+  const posterURL ="https://picsum.photos/seed/picsum/200/300";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
   return (
     <div className="App">
       <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={"Example"} genres={["action", "science"]} posterSrc={posterURL}/>
     </div>
   );
 }
